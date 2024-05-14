4dea35d429775f23444ed4213cf164edd8e84bc5
flore-jan smeets <flore-jan_smeets@hotmail.com>
2024-05-14 19:04:33 +0200

    header auth toegepast

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -15,7 +15,7 @@
             <li><a href="{{ route('club.index') }}">Clubinfo</a></li>
             <li><a href="{{ route('club.contact') }}">Contact</a></li>
             <li class="dropdown">
-                <a href="#">Wedstrijden</a>
+                <a href="#">Wedstrijden <span class="dropdown-arrow">&#x25BC;</span></a>
                 <ul class="dropdown-content">
                     <li><a href="{{ route('calenders.index') }}">Kalender</a></li>
                     <li><a href="{{ route('rankings.index') }}">Klassement</a></li>
@@ -23,13 +23,24 @@
                     <li><a href="{{ route('cards.index') }}">Kaarten</a></li>
                 </ul>
             </li>
+            @guest
+                <li><a href="{{ route('login') }}" class="auth-button">Admin</a></li>
+            @endguest
+            @auth
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button type="submit" class="auth-button">Logout</button>
+                    </form>
+                </li>
+            @endauth
         </ul>
         <ul>
-            <li><a href="{{ route('home') }}">Voetbalclub</a></li>
+            <li><a href="{{ route('home') }}">K.V.V. Heusden-Zolder</a></li>
             <li class="hideOnMobile"><a href="{{ route('club.index') }}">Clubinfo</a></li>
             <li><a href="{{ route('club.contact') }}">Contact</a></li>
             <li class="dropdown hideOnMobile">
-                <a href="#">Wedstrijden</a>
+                <a href="#">Wedstrijden <span class="dropdown-arrow">&#x25BC;</span></a>
                 <ul class="dropdown-content">
                     <li><a href="{{ route('calenders.index') }}">Kalender</a></li>
                     <li><a href="{{ route('rankings.index') }}">Klassement</a></li>
@@ -37,6 +48,17 @@
                     <li><a href="{{ route('cards.index') }}">Kaarten</a></li>
                 </ul>
             </li>
+            @guest
+                <li class="hideOnMobile"><a href="{{ route('login') }}" class="auth-button">Admin</a></li>
+            @endguest
+            @auth
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button type="submit" class="auth-button">Logout</button>
+                    </form>
+                </li>
+            @endauth
             <li class="menu-button" onclick="showSidebar()">
                 <a href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26">
