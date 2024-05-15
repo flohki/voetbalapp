98c67216e69368966c712e1dbd4dd90fe8b55c3e
flore-jan smeets <flore-jan_smeets@hotmail.com>
2024-05-15 10:25:17 +0200

    footer.css meer resonsive gemaakt en nieuws.css aangepast

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') / KVV Heusden-Zolder</title>
+    <meta name="description"
+        content="Welkom op de officiële website van K.V.V. Heusden-Zolder. Hier vind je het laatste nieuws, wedstrijdschema's, teaminformatie en meer over onze voetbalclub.">
+    <meta name="keywords"
+        content="K.V.V. Heusden-Zolder, voetbalclub, voetbal, wedstrijden, nieuws, Heusden-Zolder, sport">
+    <meta name="author" content="Flore-Jan Smeets">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @yield('styles') <!-- Hier kunnen pagina-specifieke CSS-bestanden worden toegevoegd -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link
